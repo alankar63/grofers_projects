78731ab049a3fc3a166e2095a9144ba4584a58ca
alankar <alankar63@gmail.com>
2016-06-04 09:40:33 +0530

    2nd change

---
+++ sorted_rating.sh
@@ -0,0 +1,30 @@
+echo enter directory which contains movie folder
+read dir
+final_rate.txt
+for f in $dir/movies/*
+ do
+    oname=${f##*/}
+   # echo $oname
+   name=$(echo ${oname//./+})
+   name=$(echo ${name//_/+})
+   name=$(echo ${name// /+})
+
+   curl -s  http://www.imdb.com/find\?ref_\=nv_sr_fn\&q\=$name\&s\=all |grep -E -0  '/tt\w+' |cut -c72- >out.txt
+
+   cut -c-9 out.txt >  final.txt
+
+   read val <final.txt
+
+   curl -s http://www.imdb.com/title/$val/ >rating.html
+
+   data=$(grep "based on" rating.html|grep "[0-9]\.[0-9]" -o)
+  
+   data1=${data:0:3}
+
+   printf $data1 >>final_rate.txt
+   printf " " >> final_rate.txt
+   echo $oname >> final_rate.txt
+
+done
+
+sort final_rate.txt -o final_rate.txt
