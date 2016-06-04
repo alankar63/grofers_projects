a6be3ec8369d977af1cc34885c279e32b3c00a4f
alankar <alankar63@gmail.com>
2016-06-04 16:36:51 +0530

    5th correction

--- sorted_rating.sh
+++ sorted_rating.sh
@@ -2,7 +2,7 @@
 
 if [ -z $1 ];
 then
- echo 'Directory not provided. Searching in current directory'
+ echo 'Directory not provided.Searching in current directory'
  dir='.'
 else
  dir=$1
@@ -14,16 +14,15 @@ for f in $dir/*
    name=$(echo ${orig_name//./+})
    name=$(echo ${name//_/+})
    name=$(echo ${name// /+})
-   val="$(curl -s  http://www.imdb.com/find\?ref_\=nv_sr_fn\&q\=$name\&s\=all \
-     |grep -o  -E -0  '/tt\w+' |head -1 |cut -c2- )"
-     #movie id identified
-   data=$(curl -s http://www.imdb.com/title/$val/ |grep "based on" \
-     | grep "[0-9]\.[0-9]" -o )
+   val="$(curl -s http://www.imdb.com/find\?ref_\=nv_sr_fn\&q\=$name\&s\=all \
+     | grep -o -E -0 '/tt\w+' | head -1 | cut -c2- )" # movie id identified
+   data="$(curl -s http://www.imdb.com/title/$val/ | grep "based on" \
+     | grep "[0-9]\.[0-9]" -o )"
    data1=${data:0:3}    # rating kept in data
    
    printf "$data1" 
    printf " " 
    echo $orig_name 
 
- done |sort -k1 -rn
+ done | sort -k1 -rn
 
